b1ffcd94d4a4f28f1ec0ebec8d987a92f8578a5a
MariKakuchaia <94448857+MariKakuchaia@users.noreply.github.com>
2022-03-03 17:00:35 +0400

    create food-items content in JS

---
+++ main.js
@@ -0,0 +1,555 @@
+const foodItem=[
+    {
+        id:1,
+        name: "Caprese",
+        category: "italian",
+        rating:4.3,
+        price:13,
+        img:"images/italian/caprese.jpg",
+        quantity:1
+    },
+    {
+        id:2,
+        name: "Italian",
+        category: "italian",
+        rating:4.3,
+        price:13,
+        img:"images/italian/italian1.jpg",
+        quantity:1
+    },
+    {
+        id:3,
+        name: "Lazania",
+        category: "italian",
+        rating:4.3,
+        price:13,
+        img:"images/italian/lazania.jpg",
+        quantity:1
+    },
+    {
+        id:4,
+        name: "Pizza margarita",
+        category: "italian",
+        rating:4.3,
+        price:13,
+        img:"images/italian/pizza margarita.jpg",
+        quantity:1
+    },
+    {
+        id:5,
+        name: "Pizza peperoni",
+        category: "italian",
+        rating:4.3,
+        price:13,
+        img:"images/italian/Pizza peperoni.jpg",
+        quantity:1
+    },
+    {
+        id:6,
+        name: "Ravioli",
+        category: "italian",
+        rating:4.3,
+        price:13,
+        img:"images/italian/ravioli.jpg",
+        quantity:1
+    },
+    {
+        id:7,
+        name: "Rigation pasta",
+        category: "italian",
+        rating:4.3,
+        price:13,
+        img:"images/italian/rigatoni pasta with vodka sauce.jpg",
+        quantity:1
+    },
+    {
+        id:8,
+        name: "Tiramisu",
+        category: "italian",
+        rating:4.3,
+        price:13,
+        img:"images/italian/tiramisu.jpg",
+        quantity:1
+    },
+    {
+        id:9,
+        name: "Airan",
+        category: "turkish",
+        rating:4.3,
+        price:13,
+        img:"images/turkish/airan.jpg",
+        quantity:1
+    },
+    {
+        id:10,
+        name: "Baklava",
+        category: "turkish",
+        rating:4.3,
+        price:13,
+        img:"images/turkish/baklava.jpg",
+        quantity:1
+    },
+    {
+        id:11,
+        name: "Doner kabab",
+        category: "turkish",
+        rating:4.3,
+        price:13,
+        img:"images/turkish/doner kebab.jpg",
+        quantity:1
+    },
+    {
+        id:12,
+        name: "Lentil soup",
+        category: "turkish",
+        rating:4.3,
+        price:13,
+        img:"images/turkish/lentil soup.jpg",
+        quantity:1
+    },
+    {
+        id:13,
+        name: "Pide",
+        category: "turkish",
+        rating:4.3,
+        price:13,
+        img:"images/turkish/pide.jpg",
+        quantity:1
+    },
+    {
+        id:14,
+        name: "Simit",
+        category: "turkish",
+        rating:4.3,
+        price:13,
+        img:"images/turkish/simit.jpg",
+        quantity:1
+    },
+    {
+        id:15,
+        name: "Acharuli khachapuri",
+        category: "georgian",
+        rating:4.3,
+        price:13,
+        img:"images/georgian/acharuli khachapuri.jpg",
+        quantity:1
+    },
+    {
+        id:16,
+        name: "Felamushi",
+        category: "georgian",
+        rating:4.3,
+        price:13,
+        img:"images/georgian/felamushi.jpg",
+        quantity:1
+    },
+    {
+        id:17,
+        name: "Fkhali",
+        category: "georgian",
+        rating:4.3,
+        price:13,
+        img:"images/georgian/fkhali.jpg",
+        quantity:1
+    },
+    {
+        id:18,
+        name: "Gozinaki",
+        category: "georgian",
+        rating:4.3,
+        price:13,
+        img:"images/georgian/gozinaki.jpg",
+        quantity:1
+    },
+    {
+        id:19,
+        name: "Khinkali",
+        category: "georgian",
+        rating:4.3,
+        price:13,
+        img:"images/georgian/khinkali.jpg",
+        quantity:1
+    },
+    {
+        id:20,
+        name: "Megruli khachapuri",
+        category: "georgian",
+        rating:4.3,
+        price:13,
+        img:"images/georgian/megruli khachapuri.jpg",
+        quantity:1
+    },
+    {
+        id:21,
+        name: "Sacivi",
+        category: "georgian",
+        rating:4.3,
+        price:13,
+        img:"images/georgian/sacivi.jpg",
+        quantity:1
+    },
+    {
+        id:22,
+        name: "soup kharcho",
+        category:"georgian",
+        rating:4.3,
+        price:13,
+        img:"images/georgian/soup kharcho.jpg",
+        quantity:1
+    },
+    {
+        id:23,
+        name: "Creme brule",
+        category: "french",
+        rating:4.3,
+        price:13,
+        img:"images/french/creme brule.jpg",
+        quantity:1
+    },
+    {
+        id:24,
+        name: "Duck confit",
+        category: "french",
+        rating:4.3,
+        price:13,
+        img:"images/french/duck confit.jpg",
+        quantity:1
+    },
+    {
+        id:25,
+        name: "Julienne",
+        category: "french",
+        rating:4.3,
+        price:13,
+        img:"images/french/julienne.jpg",
+        quantity:1
+    },
+    {
+        id:26,
+        name: "Kruasani",
+        category: "french",
+        rating:4.3,
+        price:13,
+        img:"images/french/kruasani.jpg",
+        quantity:1
+    },
+    {
+        id:27,
+        name: "Macaroni",
+        category: "french",
+        rating:4.3,
+        price:13,
+        img:"images/french/macaroni.webp",
+        quantity:1
+    },
+    {
+        id:28,
+        name: "Ratatoui",
+        category: "french",
+        rating:4.3,
+        price:13,
+        img:"images/french/ratatoui.jpg",
+        quantity:1
+    },
+    {
+        id:29,
+        name: "Soufle",
+        category: "french",
+        rating:4.3,
+        price:13,
+        img:"images/french/soufle.jpg",
+        quantity:1
+    },
+    {
+        id:30,
+        name: "Tarte tatin",
+        category: "french",
+        rating:4.3,
+        price:13,
+        img:"images/french/tarte tatin.jpg",
+        quantity:1
+    },
+    {
+        id:31,
+        name: "Chown mei",
+        category: "chinese",
+        rating:4.3,
+        price:13,
+        img:"images/chinese/chown mei.jpg",
+        quantity:1
+    },
+    {
+        id:32,
+        name: "Dumplings",
+        category: "chinese",
+        rating:4.3,
+        price:13,
+        img:"images/chinese/dumplings.jpg",
+        quantity:1
+    },
+    {
+        id:33,
+        name: "Mafe",
+        category: "chinese",
+        rating:4.3,
+        price:13,
+        img:"images/chinese/mafe.jpg",
+        quantity:1
+    },
+    {
+        id:34,
+        name: "Mantou",
+        category: "chinese",
+        rating:4.3,
+        price:13,
+        img:"images/chinese/mantou.jpg",
+        quantity:1
+    },
+    {
+        id:35,
+        name: "Shumai",
+        category: "chinese",
+        rating:4.3,
+        price:13,
+        img:"images/chinese/shumai.jpg",
+        quantity:1
+    },
+    {
+        id:36,
+        name: "Spring roll",
+        category: "chinese",
+        rating:4.3,
+        price:13,
+        img:"images/chinese/spring roll.jpg",
+        quantity:1
+    },
+    {
+        id:37,
+        name: "Sweet pork",
+        category: "chinese",
+        rating:4.3,
+        price:13,
+        img:"images/chinese/sweet pork.jpg",
+        quantity:1
+    },
+    {
+        id:38,
+        name: "Wonton",
+        category: "chinese",
+        rating:4.3,
+        price:13,
+        img:"images/chinese/wonton.jpg",
+        quantity:1
+    },
+];
+console.log(foodItem);
+
+function displyItem(){
+    var italian =document.getElementById("italian");
+    var turkish=document.getElementById("turkish");
+    var georgian=document.getElementById("georgian");
+    var french=document.getElementById("french");
+    var chinese=document.getElementById("chinese");
+
+    var italianData=foodItem.filter(item=>item.category=="italian");
+    console.log(italianData);
+    var turkishData=foodItem.filter(item=>item.category=="turkish")
+    console.log(turkishData);
+    var georgianData=foodItem.filter(item=>item.category=="georgian");
+    console.log(georgianData);
+    var frenchData=foodItem.filter(item=>item.category=="french");
+    console.log(frenchData);
+    var chineseData=foodItem.filter(item=>item.category=="chinese");
+    console.log(chineseData);
+
+    italianData.map(item=>{
+        var itemCard=document.createElement("div");
+        itemCard.setAttribute("id","item-card");
+
+        var cardTop=document.createElement("div");
+        cardTop.setAttribute("id","card-top");
+
+        var star=document.createElement("i");
+        star.setAttribute("class","fa fa start");
+        star.setAttribute("id","rating");
+        star.innerText=""+item.rating;
+
+        var heart=document.createElement("i");
+        heart.setAttribute("class","fa fa-heart-o add-to-cart");
+        heart.setAttribute("id",item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img=document.createElement("img");
+        img.src=item.img;
+
+        var itemName=document.createElement("p");
+        itemName.setAttribute("id", "item-name");
+        itemName.innerText=item.name
+
+        var itemPrice=document.createElement("p");
+        itemPrice.setAttribute("id", "item-price");
+        itemPrice.innerText="price:"+item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        italian.appendChild(itemCard);
+    })
+
+    turkishData.map(item=>{
+        var itemCard=document.createElement("div");
+        itemCard.setAttribute("id","item-card");
+
+        var cardTop=document.createElement("div");
+        cardTop.setAttribute("id","card-top");
+
+        var star=document.createElement("i");
+        star.setAttribute("class","fa fa start");
+        star.setAttribute("id","rating");
+        star.innerText=""+item.rating;
+
+        var heart=document.createElement("i");
+        heart.setAttribute("class","fa fa-heart-o add-to-cart");
+        heart.setAttribute("id",item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img=document.createElement("img");
+        img.src=item.img;
+
+        var itemName=document.createElement("p");
+        itemName.setAttribute("id", "item-name");
+        itemName.innerText=item.name
+
+        var itemPrice=document.createElement("p");
+        itemPrice.setAttribute("id", "item-price");
+        itemPrice.innerText="price:"+item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+       turkish.appendChild(itemCard);
+    })
+
+    georgianData.map(item=>{
+        var itemCard=document.createElement("div");
+        itemCard.setAttribute("id","item-card");
+
+        var cardTop=document.createElement("div");
+        cardTop.setAttribute("id","card-top");
+
+        var star=document.createElement("i");
+        star.setAttribute("class","fa fa start");
+        star.setAttribute("id","rating");
+        star.innerText=""+item.rating;
+
+        var heart=document.createElement("i");
+        heart.setAttribute("class","fa fa-heart-o add-to-cart");
+        heart.setAttribute("id",item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img=document.createElement("img");
+        img.src=item.img;
+
+        var itemName=document.createElement("p");
+        itemName.setAttribute("id", "item-name");
+        itemName.innerText=item.name
+
+        var itemPrice=document.createElement("p");
+        itemPrice.setAttribute("id", "item-price");
+        itemPrice.innerText="price:"+item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+       georgian.appendChild(itemCard);
+    })
+
+    frenchData.map(item=>{
+        var itemCard=document.createElement("div");
+        itemCard.setAttribute("id","item-card");
+
+        var cardTop=document.createElement("div");
+        cardTop.setAttribute("id","card-top");
+
+        var star=document.createElement("i");
+        star.setAttribute("class","fa fa start");
+        star.setAttribute("id","rating");
+        star.innerText=""+item.rating;
+
+        var heart=document.createElement("i");
+        heart.setAttribute("class","fa fa-heart-o add-to-cart");
+        heart.setAttribute("id",item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img=document.createElement("img");
+        img.src=item.img;
+
+        var itemName=document.createElement("p");
+        itemName.setAttribute("id", "item-name");
+        itemName.innerText=item.name
+
+        var itemPrice=document.createElement("p");
+        itemPrice.setAttribute("id", "item-price");
+        itemPrice.innerText="price:"+item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+       french.appendChild(itemCard);
+    })
+
+    chineseData.map(item=>{
+        var itemCard=document.createElement("div");
+        itemCard.setAttribute("id","item-card");
+
+        var cardTop=document.createElement("div");
+        cardTop.setAttribute("id","card-top");
+
+        var star=document.createElement("i");
+        star.setAttribute("class","fa fa start");
+        star.setAttribute("id","rating");
+        star.innerText=""+item.rating;
+
+        var heart=document.createElement("i");
+        heart.setAttribute("class","fa fa-heart-o add-to-cart");
+        heart.setAttribute("id",item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img=document.createElement("img");
+        img.src=item.img;
+
+        var itemName=document.createElement("p");
+        itemName.setAttribute("id", "item-name");
+        itemName.innerText=item.name
+
+        var itemPrice=document.createElement("p");
+        itemPrice.setAttribute("id", "item-price");
+        itemPrice.innerText="price:"+item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+       chinese.appendChild(itemCard);
+    })
+}
+displyItem();
